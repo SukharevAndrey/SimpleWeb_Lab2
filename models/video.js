c555e10bf41db502ae63d478ad3bfbf3defd20c0
Shiz0 <andreisukharev@gmail.com>
2015-10-29 15:43:19 +0300

    Declared video model

--- models/video.js
+++ models/video.js
@@ -1,2 +1,14 @@
 'use strict';
 
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Video = new Schema({
+    uploadedBy: Schema.Types.ObjectId,
+    name: String,
+    description: String,
+    storePath: String, // Absolute path where file is stored
+    uploadDate: Schema.Types.Date
+});
+
+module.exports = mongoose.model('Video', Video);
